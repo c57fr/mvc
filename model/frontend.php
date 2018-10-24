de84332f33add0649a533d219a52cd072a0251b1
COTE <GrCOTE7@GMail.com>
2018-10-24 14:35:03 +0200

    Add gestion des erreurs

--- model/frontend.php
+++ model/frontend.php
@@ -11,8 +11,8 @@ function getPosts()
 function getPost($postId)
 {
   $db = dbConnect();
-  $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
-  $req->execute(array ($postId));
+  $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %H:%i:%ss\') AS creation_date_fr FROM posts WHERE id = ?');
+  $req->execute(array($postId));
   $post = $req->fetch();
 
   return $post;
@@ -21,8 +21,8 @@ function getPost($postId)
 function getComments($postId)
 {
   $db = dbConnect();
-  $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
-  $comments->execute(array ($postId));
+  $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %H:%i:%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
+  $comments->execute(array($postId));
 
   return $comments;
 }
@@ -31,20 +31,13 @@ function postComment($postId, $author, $comment)
 {
   $db = dbConnect();
   $comments = $db->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
-  $affectedLines = $comments->execute(array ($postId, $author, $comment));
+  $affectedLines = $comments->execute(array($postId, $author, $comment));
 
   return $affectedLines;
 }
 
 function dbConnect()
 {
-  try
-  {
     $db = new PDO('mysql:host=localhost;dbname=mvc;charset=utf8', 'root', '');
     return $db;
-  }
-  catch (Exception $e)
-  {
-    die ('Erreur : ' . $e->getMessage());
-  }
 }
