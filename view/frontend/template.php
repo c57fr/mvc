0da3059ae89f956a28fb569dff685b335a384507
COTE <GrCOTE7@GMail.com>
2018-10-24 17:02:59 +0200

    Transform en POO

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -8,5 +8,22 @@
         
     <body>
         <?= $content ?>
+        
+        <a href="https://outils-javascript.aliasdmc.fr/encodage-caracteres-emoji/encode-caractere-1F635-html-css-js-autre.html" target="_blank">😃</a>
+        <hr>
+        <!--
+        &#x1F635;
+        &#x1F614;
+        ;-)
+        &#x1F609;
+        Win: Avec Clavier tactile
+        -->
+        <div class="loader">
+   
+          <svg version="1.1"  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="50px" y="0px" viewBox="-466.4 259.6 280.2 47.3" enable-background="new -466.4 259.6 280.2 47.3" xml:space="preserve">
+          <polyline class="animation"   points="-465.4,281 -436,281 -418.9,281 -423.9,281 -363.2,281 -355.2,269 -345.2,303 -335.2,263 -325.2,291 -319.2,281 -187.2,281 "></polyline>
+       
+        </svg>
+      </div>
     </body>
 </html>
