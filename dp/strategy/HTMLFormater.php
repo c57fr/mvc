6d9fc700765ab5b7f6816a6a6a1eabd0480d841c
COTE <GrCOTE7@GMail.com>
2018-10-28 13:14:34 +0100

    Add heure dans Strategy

--- dp/strategy/HTMLFormater.php
+++ dp/strategy/HTMLFormater.php
@@ -3,6 +3,6 @@ class HTMLFormater implements Formater
 {
  public function format($text)
  {
-  return '<p>Date : ' . date('d/m/Y',time()) . '<br />' . "\n" . 'Texte : ' . $text . '</p>';
+  return '<p>Date : ' . date('d/m/Y - h:i:s',time()) . '<br />' . "\n" . 'Texte : ' . $text . '</p>';
  }
 }
