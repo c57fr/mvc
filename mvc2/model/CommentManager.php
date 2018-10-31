85b9eb8909da936fe7e9d7bd14d0a685646096de
COTE <GrCOTE7@GMail.com>
2018-10-31 10:35:11 +0100

    Add pub c57.fr

--- mvc2/model/CommentManager.php
+++ mvc2/model/CommentManager.php
@@ -8,8 +8,8 @@ class CommentManager extends Manager
 {
  public function getComments($postId)
  {
-  $db       = $this->dbConnect();
-  $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
+  $dbc       = $this->dbConnect();
+  $comments = $dbc->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
   $comments->execute(array($postId));
 
   return $comments;
@@ -17,8 +17,8 @@ class CommentManager extends Manager
 
  public function postComment($postId, $author, $comment)
  {
-  $db            = $this->dbConnect();
-  $comments      = $db->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
+  $dbc           = $this->dbConnect();
+  $comments      = $dbc->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
   $affectedLines = $comments->execute(array($postId, $author, $comment));
 
   return $affectedLines;
--- mvc2/view/frontend/listPostsView.php
+++ mvc2/view/frontend/listPostsView.php
@@ -5,11 +5,15 @@ ob_start();?>
 <h1>Mon super blog !</h1>
 <p>Derniers billets du blog :</p>
 
+<p style="text-align: center">Pour apprendre en co-créant:</p>
+<p style="text-align: center; font-size:1.5em; font-weight: bold; font-family: 'Comic Sans MS', arial; padding: 10px; background-color: orange; border-radius: 7px"><a style=" text-decoration: none;" href="http://c57.fr" target="_blank"> c57.fr</a></p>
+
+<!--
 <hr>
 <a href="./doc" target="_blank">Doc</a>
 <p>Générée avec composer update, puis: vendor\bin\phpdoc.php.bat -d ./mvc2 -t ./mvc2/doc</p>
-
 <hr>
+-->
 
 <?php
 while ($data = $posts->fetch()) {
